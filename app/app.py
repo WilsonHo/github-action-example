eaface76f0c0e1270ad79766c5dda239cf799b4c
Bao Ho <bao.ho@tookitaki.com>
2023-03-10 18:57:05 +0800

    need to rebuid

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! I am a docker container 1"
+    return "Hello, World! I am a docker container need to rebuid"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
